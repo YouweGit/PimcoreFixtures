e686b0922ce4e5307c0033913df47531380b029e
Joris Ros <joris.ros@gmail.com>
2018-01-07 05:43:16 +0100

    Add the correct use for loading classes

--- Command/GenerateFixturesCommand.php
+++ Command/GenerateFixturesCommand.php
@@ -2,12 +2,16 @@
 
 namespace FixtureBundle\Command;
 
+use FixtureBundle\Service\FixtureLoader;
 use Pimcore\Console\AbstractCommand;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Validator\Constraints\Choice;
 
 class GenerateFixturesCommand extends AbstractCommand
 {
